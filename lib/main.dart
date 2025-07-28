071329686244b1817ae00b67f5fe90ccb251849b
masudrr60 <masudrr60@gmail.com>
2025-07-28 21:55:34 +0600

    initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'Home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,96 +16,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Rahat"),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children:[ Container(
-            height: 200,
-            width: 300,
-            color: Colors.blue,
-            child: FractionallySizedBox(
-              alignment: Alignment.topLeft,
-              heightFactor: 0.5,
-              widthFactor: 0.8,
-              child: Container(
-                color: Colors.red,
-              ),
-            ),
-          ),
-        SizedBox(
-          height: 10,
-        ),
-          SizedBox(
-            height: 50,
-            width: 30,
-            child: Column(
-              children: [
-                Expanded(
-                  flex:1,
-                  child: Container(
-                    color: Colors.red,
-                  ),
-                ),
-                Expanded(
-                  flex: 2,
-                  child: Container(
-                    color: Colors.blue,
-                  ),
-                )
-              ],
-            ),
-          ),
-SizedBox(
-  height: 20,
-),
-         Center(
-           child: Stack(
-             children: [
-               Container(
-                 height: 400,
-                 width: 300,
-                 color: Colors.red,
-               ),
-               Container(
-                 height: 140,
-                 width: 130,
-                 color: Colors.blue,
-               ),
-               Positioned(
-                 bottom: 10,
-                 right: 5,
-                 child: Container(
-                   height: 130,
-                   width:120,
-                   color: Colors.yellow,
-                 ),
-               ),
-             ],
-           ),
-         ),
-            SizedBox(
-              height: 40,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
